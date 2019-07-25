2a1baf1f27cf832b4894367df0dfef544fa6b3f0
Marius Berger <mberger@evoila.de>
2019-07-25 15:05:49 +0200

    chore(CassandraDbService): Commit current dirty state for alter use.
    ! This commit is not meant to be a complete state, fix or such a thing !
    ! It is meant to store the current work, because the Java Upgrade of the osb-cassandra is pushed back for more important brokers !

--- osb-service/src/main/java/de/evoila/cf/broker/custom/cassandra/CassandraDbService.java
+++ osb-service/src/main/java/de/evoila/cf/broker/custom/cassandra/CassandraDbService.java
@@ -1,11 +1,12 @@
 package de.evoila.cf.broker.custom.cassandra;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import de.evoila.cf.broker.model.catalog.ServerAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetSocketAddress;
 import java.util.List;
 
 /**
@@ -15,16 +16,17 @@ public class CassandraDbService {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    private Session session;
+    private CqlSession session;
 
     public boolean createConnection(String username, String password, String database, List<ServerAddress> serverAddresses) {
         try {
             String keyspace = database;
-            Cluster cluster = Cluster.builder()
-                    .addContactPoints(serverAddresses.stream().map(s -> s.getIp()).toArray(String[]::new))
-                    .withCredentials(username, password)
-                    .build();
-            session = cluster.connect(keyspace);
+
+            CqlSessionBuilder sessionBuilder = CqlSession.builder();
+            for (ServerAddress sA : serverAddresses)
+                sessionBuilder.addContactPoint(new InetSocketAddress(sA.getIp(), sA.getPort()));
+
+            session = sessionBuilder.withKeyspace(keyspace).build();
         } catch (Exception e) {
             log.info("Could not establish client", e);
             return false;
