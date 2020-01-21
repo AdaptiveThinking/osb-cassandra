1b421fe59c6d89268791c06f3c01d6d206b9619f
Johannes Strauß <jstrauss@evoila.de>
2020-01-21 09:17:24 +0100

    fix(CassandraUtils): Don't return null when no UUID was given
    Throw IllegalArgumentException in case a non UUID string was given as a parameter to method dbName.
    
    Break: needs osb-core PR:99  to be approved!
    https://github.com/evoila/osb-core/pull/99

--- osb-service/src/main/java/de/evoila/cf/broker/custom/cassandra/CassandraUtils.java
+++ osb-service/src/main/java/de/evoila/cf/broker/custom/cassandra/CassandraUtils.java
@@ -1,17 +1,19 @@
 package de.evoila.cf.broker.custom.cassandra;
 
+import de.evoila.cf.broker.util.UuidUtils;
+
 /**
- * @author Johannes Hiemer.
+ * @author Johannes Hiemer, Johannes Strauß.
  */
 public class CassandraUtils {
 
     public static String dbName(String uuid) {
-        if (uuid != null && uuid.length() > 15)
-            return "d" + uuid.replace("-", "")
-                    .substring(0, 15)
-                    .toLowerCase();
-        else
-            return null;
-    }
+        if (!UuidUtils.isValidUUID(uuid)) {
+            throw new IllegalArgumentException("Please use valid UUIDs to create Cassandra database names!");
+        }
 
+        return "d" + uuid.replace("-", "")
+                .substring(0, 15)
+                .toLowerCase();
+    }
 }
